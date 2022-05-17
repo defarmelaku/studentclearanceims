4d6daea2654edec03b15e12fe91fa03e45dbef07
fantansh <kaleabe1221@gmail.com>
2022-05-17 14:40:33 +0100

    mode and migrtion created for proctor

---
+++ app/Models/proctor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class proctor  extends Model
+{
+    use HasFactory;
+    protected  $fillablele = ['stu_name','stu_id','dorm_matrial'];
+
+
+}
